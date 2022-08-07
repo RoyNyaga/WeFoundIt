0b871849f6d19a63ceadbfa93360d72aff6fc25c
RoyNyaga <nyagaandreroy@gmail.com>
2022-08-07 20:50:29 +0100

    style accoutn form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,24 @@
 class UsersController < ApplicationController
+
+  def edit
+    @user = current_user
+  end
+
   def show
     @user = User.find_by(id: params[:id])
+  end
+
+  def update
+    @user = User.find_by(id: params[:id])
+    if @user.update(user_params)
+      flash[:success] = "You have successfully updated your account."
+      redirect_to user_path(@user)
+    else
+      render "edit"
+    end
+  end
+
+  def user_params
+    params.require(:user).permit(:full_name, :email, :phone, :avatar, :region, :city)
   end
 end
