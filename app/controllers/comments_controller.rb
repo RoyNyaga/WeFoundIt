7f6216e904d953f5f791effad211a8e35388469c
RoyNyaga <nyagaandreroy@gmail.com>
2022-08-10 12:02:17 +0100

    fix nil message error

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -19,6 +19,6 @@ class CommentsController < ApplicationController
   end
 
   def comment_params
-    params.require(:comment).permit(:missing_item_id, :user_id)
+    params.require(:comment).permit(:missing_item_id, :user_id, :message)
   end
 end
