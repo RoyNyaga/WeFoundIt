e63fa6f4327a9acc88ea671a03b2934259c12f7e
RoyNyaga <nyagaandreroy@gmail.com>
2022-08-10 10:59:00 +0100

    add comment actions

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,24 @@
 class CommentsController < ApplicationController
+
+  def update
+    @missing_item = MissingItem.find_by(id: params[:comment][:missing_item_id])
+    @comment = Comment.find_by(id: params[:id])
+    if @comment.update(comment_params)
+      flash[:notice] = "Comment was successfully Updated"
+      redirect_to @missing_item
+    end
+  end
+
+  def create
+    @missing_item = MissingItem.find_by(id: params[:comment][:missing_item_id])
+    @comment = Comment.new(comment_params)
+    if @comment.save?
+      flash[:notice] = "Comment was successfully Added"
+      redirect_to @missing_item
+    end
+  end
+
+  def comment_params
+    params.require(:comment).permit(:missing_item_id, :user_id)
+  end
 end
